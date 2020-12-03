1f74809920cd71f6d8d804e8dafd38a58f6cf284
John McLevey <johnmclevey@Johns-MacBook-Pro.local>
2020-12-03 09:40:28 -0330

    routine update

--- update_website.sh
+++ update_website.sh
@@ -2,15 +2,15 @@
 
 echo -e "\033[0;32mBuilding site.\033[0m"
 
-jekyll build
+jekyll build --trace
 
 echo -e "\033[0;32mCopying to git repo.\033[0m"
 FILES=_site/*
 for f in $FILES
 do
   echo "Processing $f file..."
-  cp $f /Users/johnmclevey/Documents/websites/mclevey.github.io/
+  cp $f /Users/johnmclevey/Documents/work/career/websites/mclevey.github.io/
 done
 
 echo -e "\033[0;32mPushing changes.\033[0m"
-cd /Users/johnmclevey/Documents/websites/mclevey.github.io/ && git add . && git commit -m 'routine update' && git push
+cd /Users/johnmclevey/Documents/work/career/websites/mclevey.github.io/ && git add . && git commit -m 'routine update' && git push
